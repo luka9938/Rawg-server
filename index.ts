cc426b7b77adcb0928b19e148866d55ef4ed4751
Tomas Pesek <pesekt@gmail.com>
2024-02-05 13:03:28 +0100

    -

--- index.ts
+++ index.ts
@@ -2,10 +2,12 @@ import express from "express";
 import dbConnect from "./startup/dbConnect";
 import "dotenv/config";
 import todoRouter from "./routers/todoRouter";
+import cors from "cors";
 
 dbConnect();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use("/todos", todoRouter);
 
